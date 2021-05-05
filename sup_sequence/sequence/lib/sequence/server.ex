cde649306125e850a224c848ac06c19d09f3dbec
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-05-06 07:54:46 +0900

    18.1

--- sup_sequence/sequence/lib/sequence/server.ex
+++ sup_sequence/sequence/lib/sequence/server.ex
@@ -4,8 +4,8 @@ defmodule Sequence.Server do
   #####
   # External API  
 
-  def start_link(current_number) do
-    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def next_number do
@@ -19,8 +19,8 @@ defmodule Sequence.Server do
   #####
   # GenServer implementation
 
-  def init(initial_number) do
-    { :ok, initial_number }
+  def init(_) do
+    { :ok, Sequence.Stash.get() }
   end
   
   def handle_call(:next_number, _from, current_number) do
@@ -29,6 +29,10 @@ defmodule Sequence.Server do
 
   def handle_cast({:increment_number, delta}, current_number) do
     { :noreply, current_number + delta}
+  end
+
+  def terminate(_reason, current_number) do
+    Sequence.Stash.update(current_number)
   end
 
   def format_status(_reason, [ _pdict, state ]) do
